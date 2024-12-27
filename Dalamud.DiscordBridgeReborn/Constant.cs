45cf43f27e4044920fc76b63c5bd8ef02c814929
Raiden <153776281+RaidenvBlack@users.noreply.github.com>
2024-12-27 19:20:25 +0100

    updated Constant

--- Dalamud.DiscordBridgeReborn/Constant.cs
+++ Dalamud.DiscordBridgeReborn/Constant.cs
@@ -8,9 +8,9 @@ namespace Dalamud.DiscordBridge
 {
     static class Constant
     {
-        public const string HelpLink = "https://github.com/reiichi001/Dalamud.DiscordBridge/wiki/Setup-Guide";
-        public const string KindListLink = "https://github.com/reiichi001/Dalamud.DiscordBridge/wiki/Chat-kinds";
-        public const string DiscordJoinLink = "https://discord.gg/3NMcUV5";
-        public const string LogoLink = "https://raw.githubusercontent.com/goatcorp/DalamudAssets/master/UIRes/logo.png";
+        public const string HelpLink = "https://github.com/SayuShira/Dalamud.DiscordBridge/wiki/Setup-Guide";
+        public const string KindListLink = "https://github.com/SayuShira/Dalamud.DiscordBridge/wiki/Chat-kinds";
+        public const string DiscordJoinLink = "https://i.imgur.com/bjdDApO.jpeg";
+        public const string LogoLink = "https://i.imgur.com/25aR2lF.png";
     }
 }
